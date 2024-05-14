1a1ae6f225e3081ea04e7e52c9a65cb21254dab6
Victor Shcherbina <98951159+temptempest@users.noreply.github.com>
2024-05-14 14:31:06 +0400

    Updated CoinImageView

--- Crypto2/Screens/Components/CoinImage/CoinImageView.swift
+++ Crypto2/Screens/Components/CoinImage/CoinImageView.swift
@@ -6,19 +6,29 @@
 //
 
 import UIKit
-
+import Combine
 final class CoinImageView: UIImageView {
 	var vm: CoinImageViewModel
-	
+	private var cancelables = Set<AnyCancellable>()
 	init(coin:Coin) {
 		vm = CoinImageViewModel(coin: coin)
 		super.init(frame: .zero)
 		self.contentMode = .scaleAspectFit
+		
 		if let image = vm.image {
 			self.image = image.resized(toWidth: 30)
 		} else {
 			self.image = UIImage(systemName: "questionmark")
 		}
+		
+		vm.$image
+			.sink { image in
+				Task { @MainActor in
+					self.image = image?.resized(toWidth: 30)
+				}
+			}
+			.store(in: &cancelables)
+		
 	}
 	
 	required init?(coder: NSCoder) {
